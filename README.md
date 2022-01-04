0e68991eac223d445168a48af394aa4cf5d2b26a
tastelikecoke <jmerc87@gmail.com>
2022-01-04 12:03:43 +0800

    Add files

---
+++ README.md
@@ -0,0 +1,6 @@
+Neejeesahnjee Website
+=====================
+
+[check out the website here.](neejeesahnjee.crabdance.com)
+
+Please don't abuse too much.
---
+++ my-little-function/index.js
@@ -0,0 +1,55 @@
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-west-2' });
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context, callback) => {
+    var result = {}
+    if(!event.hasOwnProperty('queryStringParameters'))
+    {
+        result = {"body" : "No parameter."}
+    }
+    else if(event.queryStringParameters.type == "get")
+    {
+        result = {"body": await getCount()}
+    }
+    else if(event.queryStringParameters.type == "add")
+    {
+        await addCount()
+        result = {"body": await getCount()}
+    }
+    else
+    {
+        result = {"body" : "Nothing matching."}
+    }
+    result.headers = {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    }
+    
+    result.statusCode = 200
+    callback(null, result);
+};
+
+const getCount = async (event, context) => {
+    const data = await dynamodb.get({ TableName: "my-little-table", Key: {"key": "NijiCount"}})
+    .promise()
+    
+    return data.Item.count
+}
+
+const addCount = async (event, context) => {
+    const data = await dynamodb.get({ TableName: "my-little-table", Key: {"key": "NijiCount"}})
+    .promise()
+    
+    await dynamodb.put({TableName: "my-little-table", Item: {key: "NijiCount", count: data.Item.count + 1}}).promise()
+}
+/*
+async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+}; */
